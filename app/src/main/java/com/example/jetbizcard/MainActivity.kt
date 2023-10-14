410c964fc9d7ba681e073f73b6a21ad686c9c941
Ulises Cadena <ucadena07@gmail.com>
2023-10-14 08:35:41 -0500

    code

---
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.jetbizcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+import com.example.jetbizcard.ui.theme.JetBizCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetBizCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    CreateBizCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateBizCard(){
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(12.dp),
+             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+             colors = CardDefaults.cardColors(containerColor = Color.White),
+             elevation = CardDefaults.cardElevation(4.dp))
+        {
+            Surface(modifier = Modifier
+                .size(150.dp)
+                .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(.5.dp, Color.LightGray),
+                tonalElevation = 4.dp,
+                color = MaterialTheme.colorScheme.onSurface.copy(.5f)
+            ) {
+                Image(painter = painterResource(id = R.drawable.profile_image), contentDescription = "profile image",
+                    modifier = Modifier.size(135.dp),
+                    contentScale = )
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetBizCardTheme {
+        CreateBizCard()
+    }
+}
